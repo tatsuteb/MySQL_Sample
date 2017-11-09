9a347f981751d3af5afceef5104df6033ce4901c
kinjo <tatsuteb@gmail.com>
2017-11-10 02:14:26 +0900

    First Commit

---
+++ MySQL_Sample/Data/MySQLSampleDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySQL_Sample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQL_Sample.Data
+{
+    public class MySQLSampleDbContext: DbContext
+    {
+        public DbSet<MyTest> MyTests { get; set; }
+
+        public MySQLSampleDbContext(DbContextOptions<MySQLSampleDbContext> options): base(options)
+        {
+        }
+    }
+}
---
+++ MySQL_Sample/Models/MyTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQL_Sample.Models
+{
+    public class MyTest
+    {
+        public int Id { get; set; }
+
+        [MaxLength(45)]
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
